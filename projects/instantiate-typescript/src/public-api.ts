ae9ab7eab227a6b20b4d08ecc53f2f37f55c1306
Wouter Vandenputte <woutervandenp@gmail.com>
2023-09-14 09:48:12 +0200

    Fix date decorator not exported

--- projects/instantiate-typescript/src/public-api.ts
+++ projects/instantiate-typescript/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/decorators/model.decorator';
 export * from './lib/decorators/observable.decorator';
+export * from './lib/decorators/date.decorator';
 export * from './lib/mapper/abstraction-mapper';
